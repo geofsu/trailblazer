d99bbac4455402ac55b29d6a4389ed57b43b4c1c
Ben <geo@GEOs-MacBook-Pro.local>
2016-04-24 07:44:22 -0400

    Added another possible length

--- index.js
+++ index.js
@@ -41,7 +41,12 @@ app.post('/webhook/', function (req, res) {
                 sendLess2Message(sender)
                 continue
             }
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            sendTextMessage(sender, text.substring(0, 200) + "is unavailable.")
+            if (text === '1') {
+              sendLess1message(sender)
+              continue
+            }
+            sendTextMessage(sender, text.substring(0,200) + "is unavailable")
         }
         if (event.postback) {
             text = JSON.stringify(event.postback)
@@ -107,6 +112,55 @@ function sendLess2Message(sender) {
                     "title": "Fern Trail",
                     "subtitle": "1.62 mi",
                     "image_url": "http://defencely.com/blog/wp-content/uploads/2013/06/ways-hackers-hack-your-website-e1371080108770.jpg",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "http://talgov.com/Uploads/Public/Documents/parks/pdf/ferntrail.pdf",
+                        "title": "Map of trail"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+function sendLess1Message(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Hello",
+                    "subtitle": "0.62 mi",
+                    "image_url": "http://decorriver.com/wp-content/uploads/2015/12/Hello1.jpeg",
                     "buttons": [{
                         "type": "web_url",
                         "url": "http://talgov.com/Uploads/Public/Documents/parks/pdf/ferntrail.pdf",
